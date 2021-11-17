cb6e3be7b620e957862d171e63f5dccb6e55e9ed
Miguel-4 <73200168+Miguel-4@users.noreply.github.com>
2021-11-16 19:15:08 -0500

    Enemies

--- Assets/Scritps/Bullet.cs
+++ Assets/Scritps/Bullet.cs
@@ -6,12 +6,15 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] float speed;
     int tX;
+    int eX1;
+    int eX2;
     private bool ded;
     private float timer;
     Animator myAnimator;
     // Start is called before the first frame update
     void Start()
     {
+        //Debug.Log(gameObject.name);
         myAnimator = GetComponent<Animator>();
         tX = (int)GameObject.Find("Player").transform.localScale.x;
     }
@@ -19,17 +22,44 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (ded)
-            timer += Time.deltaTime;
-        if (timer > 0.2)
-            Destroy(this.gameObject);
-        transform.Translate(new Vector2(tX * speed * Time.deltaTime, 0));
+
+        switch (gameObject.name) {
+            case "Bullet(Clone)":
+                if (ded)
+                    timer += Time.deltaTime;
+                if (timer > 0.2)
+                    Destroy(this.gameObject);
+                transform.Translate(new Vector2(tX * speed * Time.deltaTime, 0));
+            break;
+
+            case "Bullet_green_L(Clone)":
+                transform.Translate(new Vector2(-speed * Time.deltaTime, speed * Time.deltaTime));
+            break;
+
+            case "Bullet_green_R(Clone)":
+                transform.Translate(new Vector2(speed * Time.deltaTime, speed * Time.deltaTime));
+            break;
+
+            case "Bullet_red(Clone)":
+                //transform.localScale = new Vector2(1, 1);
+                transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+            break;
+
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        myAnimator.SetBool("Coll", true);
-        speed = 0;
-        ded = true;
+        switch (gameObject.name) {
+            case "Bullet(Clone)":
+                myAnimator.SetBool("Coll", true);
+                speed = 0;
+                ded = true;
+            break;
+            default:
+                speed = 0;
+                Destroy(this.gameObject);
+            break;
+        }
     }
 }
